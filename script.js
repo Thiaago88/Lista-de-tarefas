b873a5efaa455e7985c251db0322e30aa73a8e4f
Thiaago88 <thiagosales8895@gmail.com>
2025-06-04 20:31:34 -0300

    Lista de tarefas 04/06/2025

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
-function adicionarTarefas(){
-    console.log("teste aula fucionou")
+const entradaTarefa = document.getElementById("entradaTarefa");
+const listaTarefas = document.getElementById("listatarefas");
+
+let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+let posicaoEditar = undefined;
+
+carregarTarefas();
+
+function adicionarTarefa() {
+  let texto = entradaTarefa.value.trim();
+  if (texto != "") {
+    if (posicaoEditar != undefined) {
+      tarefas[posicaoEditar] = entradaTarefa.value;
+      posicaoEditar = undefined;
+    } else {
+      tarefas.push(entradaTarefa.value);
+    }
+    entradaTarefa.value = "";
+    salvarTarefas();
+    carregarTarefas();
+  } else {
+    alert("Tarefa inválida");
+  }
+}
+entradaTarefa.addEventListener("keypress", function (tecla) {
+  if (tecla.key === "Enter") {
+    adicionarTarefa();
+  }
+});
+
+function carregarTarefas() {
+  listaTarefas.innerHTML = "";
+  tarefas.forEach((tarefa, posicao) => {
+    const item = document.createElement("li");
+    item.className = "item-lista";
+    item.innerHTML = `
+    <span>${tarefa}</span>
+    <button id="botaoRemover" onclick="editarTarefa(${posicao})">Editar</button>
+    <button id="botaoRemover" onclick="removerTarefa(${posicao})">x</button>
+  `;
+    listaTarefas.appendChild(item);
+  });
+}
+function removerTarefa(posicao) {
+  tarefas.splice(posicao, 1);
+  salvarTarefas();
+  carregarTarefas();
+}
+
+function salvarTarefas() {
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
+}
+
+function editarTarefa(posicao) {
+  posicaoEditar = posicao;
+  entradaTarefa.value = tarefas[posicao];
 }
